a042fde35afcd18f5f816077e3397a5d8085e9a3
Tim <tim.knuettel@study.thws.de>
2025-10-28 15:10:45 +0100

    test: add assert statement in test

--- backend/offene-stellen-assistent/src/test/java/de/deutscherv/gq0500/offenestellenassistent/webScraping/JobLinkScraperTest.java
+++ backend/offene-stellen-assistent/src/test/java/de/deutscherv/gq0500/offenestellenassistent/webScraping/JobLinkScraperTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 @SpringBootTest
 public class JobLinkScraperTest {
 
@@ -18,5 +21,6 @@ public class JobLinkScraperTest {
         List<String> links = jobLinkScraper.fetchAllListingLinks();
         System.out.println("Open Job offers: " + links.size());
         links.forEach(System.out::println);
+        assertThat(links.size(), is(165));
     }
 }
